bce04a991a407ebb5a89b28da339baa6fcd3197c
Ravi Kumawat <49914487+ravikumawat7716@users.noreply.github.com>
2021-03-25 23:30:11 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,7 @@
+print("Simple Intrest Calculater");
+a=input("Enter the Princial Amount in INR ");
+b=input("Enter the intrest rate percent per year ");
+c=input("Enter the time in years ");
+d=float(a)*float(b)*float(c)/100;
+print("The Intrest is ",d);
+print("The total Amount to be paid/taken is",float(a)+d);
